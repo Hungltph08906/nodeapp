a176f19a31818c2c1f36ff857952199847f68657
ADMIN <hungltph08906@fpt.edu.vn>
2020-12-21 03:32:47 +0700

    new commit

--- index.js
+++ index.js
@@ -31,6 +31,7 @@ app.get('/', function (req, res) {
         'index'
     )
 });
+var checkLogin = false
 var users = [];
 firebase.database().ref('/Users/').once('value', (snapshot) => {
     snapshot.forEach((childSnapshot) => {
@@ -40,54 +41,82 @@ firebase.database().ref('/Users/').once('value', (snapshot) => {
     });
 });
 app.get('/users', function (req, res) {
-    res.render("users/index", {users: users});
+    if (checkLogin === true){
+        res.render("users/index", {users: users});
+    } else {
+        res.redirect('/')
+    }
 })
 
 app.get('/users/search', (req, res) => {
-    var name_search = req.query.name // lấy giá trị của key name trong query parameters gửi lên
+    if (checkLogin === true){
+        var name_search = req.query.name // lấy giá trị của key name trong query parameters gửi lên
 
-    var result = users.filter((user) => {
-        // tìm kiếm chuỗi name_search trong user name.
-        // Lưu ý: Chuyển tên về cùng in thường hoặc cùng in hoa để không phân biệt hoa, thường khi tìm kiếm
-        return user.name.toLowerCase().indexOf(name_search.toLowerCase()) !== -1
-    })
+        var result = users.filter((user) => {
+            // tìm kiếm chuỗi name_search trong user name.
+            // Lưu ý: Chuyển tên về cùng in thường hoặc cùng in hoa để không phân biệt hoa, thường khi tìm kiếm
+            return user.name.toLowerCase().indexOf(name_search.toLowerCase()) !== -1
+        })
 
-    res.render('users/index', {
-        users: result // render lại trang users/index với biến users bây giờ chỉ bao gồm các kết quả phù hợp
-    });
-    console.log('connected'+result.name)
+        res.render('users/index', {
+            users: result // render lại trang users/index với biến users bây giờ chỉ bao gồm các kết quả phù hợp
+        });
+        console.log('connected'+result.name)
+    } else {
+        res.redirect('/')
+    }
 })
 
 app.get('/users/create', (req, res) => {
-    res.render('users/create', {viewTitle: 'Thêm mới'})
+    if (checkLogin === true){
+        res.render('users/create', {viewTitle: 'Thêm mới'})
+    } else {
+        res.redirect('/')
+    }
 })
-var Uname,UEmail,UPassword,UPhone,UDateOfBirth,UType
+var Uname,UEmail,UPassword,UPhone,UDateOfBirth,UType,UTypeId
 app.get('/users/update/:id', (req, res) => {
 
-    var result = users.filter((user) => {
+    if (checkLogin === true){
+        users.filter((user) => {
 
-        if (user.uid === req.params.id) {
-            Uname = user.name,
-            UEmail = user.email,
-            UPassword = user.password,
-            UPhone = user.phone,
-            UDateOfBirth = user.dateOfBirth,
-            UType = user.type
+            if (user.uid === req.params.id) {
+                Uname = user.name,
+                    UEmail = user.email,
+                    UPassword = user.password,
+                    UPhone = user.phone,
+                    UDateOfBirth = user.dateOfBirth,
+                    UTypeId = user.type
+            }
+
+        })
+        if (UTypeId === "3"){
+            UType = "Admin"
         }
-
-    })
-    res.render('users/update', {
-        viewTitle: 'Sửa thông tin',
-        uid: req.params.id,
-        name: Uname,
-        email: UEmail,
-        password: UPassword,
-        phone: UPhone,
-        dateOfBirth: UDateOfBirth,
-        type: UType,
-        status: 'disabled'
-    })
-    console.log('connected')
+        if (UTypeId === "2") {
+            UType = "Student"
+        }
+        if (UTypeId === "1") {
+            UType = "Teacher"
+        }
+        if (UTypeId === "0") {
+            UType = "Trung tam"
+        }
+        res.render('users/update', {
+            viewTitle: 'Sửa thông tin',
+            uid: req.params.id,
+            name: Uname,
+            email: UEmail,
+            password: UPassword,
+            phone: UPhone,
+            dateOfBirth: UDateOfBirth,
+            type: UType,
+            status: 'disabled'
+        })
+        console.log(UType)
+    } else {
+        res.redirect('/')
+    }
 })
 
 app.listen(port, function (err, data) {
@@ -153,18 +182,22 @@ app.get("/userData/", function (req, res) {
 })
 
 app.get('/users/delete/:id', function (req, res) {
-    firebase.database().ref('/Users/' + req.params.id).remove()
-    var newUsers = []
-    users = newUsers
-    firebase.database().ref('/Users/').once('value', (snapshot) => {
-        snapshot.forEach((childSnapshot) => {
-            var childKey = childSnapshot.key;
-            var childData = childSnapshot.val();
-            users.push(childData)
+    if (checkLogin === true){firebase.database().ref('/Users/' + req.params.id).remove()
+        var newUsers = []
+        users = newUsers
+        firebase.database().ref('/Users/').once('value', (snapshot) => {
+            snapshot.forEach((childSnapshot) => {
+                var childKey = childSnapshot.key;
+                var childData = childSnapshot.val();
+                users.push(childData)
+            });
         });
-    });
-    console.log('xoa loi' + req.params.id)
-    res.redirect('/users')
+        console.log('xoa loi' + req.params.id)
+        res.redirect('/users')
+
+    } else {
+        res.redirect('/')
+    }
 });
 
 app.post('/login',
@@ -181,7 +214,8 @@ app.post('/login',
             if (condition.username.toLowerCase().indexOf(user.email.toLowerCase()) !== -1 &&
                 user.email.toLowerCase().indexOf(condition.username.toLowerCase()) !== -1 &&
                 condition.password.toLowerCase().indexOf(user.password.toLowerCase()) !== -1 &&
-                user.password.toLowerCase().indexOf(condition.password.toLowerCase()) !== -1
+                user.password.toLowerCase().indexOf(condition.password.toLowerCase()) !== -1 &&
+                user.type === "3"
             ) {
                 return condition.password.toLowerCase().indexOf(user.password.toLowerCase()) !== -1
             }
@@ -190,13 +224,16 @@ app.post('/login',
 
 
         if (result.length === 1) {
+            checkLogin = true
             res.redirect('/users')
         } else {
-            res.render('index')
+            res.redirect('/',{
+                loginFail: "Tài khoản hoặc mật khẩu không chính xác"
+            })
         }
 
 
     });
 app.get('/signout', (req, res) => {
-    res.render('index')
+    res.redirect('/')
 });
